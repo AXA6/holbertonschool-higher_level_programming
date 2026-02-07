6b57b57106dc73c5a0729de32bfb3de14bc83445
AXA6 <Abdullah97sd@gmail.com>
2026-02-07 18:59:26 +0000

    correct read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""read file my_file.txt from 0-main.py"""
+
+
+def read_file(filename=""):
+    """def file name"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        """with open"""
+        data_file = f.read()
+        print(data_file, end="")
