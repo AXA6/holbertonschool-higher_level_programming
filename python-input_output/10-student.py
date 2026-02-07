cca1be879fc380c7deded41200fb5a654c9c5355
AXA6 <Abdullah97sd@gmail.com>
2026-02-07 19:23:18 +0000

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" function class Student that defines a student by"""
+
+
+class Student:
+    """define class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """def init"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """def class to json"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
