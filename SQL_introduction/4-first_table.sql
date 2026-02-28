7817bff2c4030657944bce76fb5f5d84c89029e3
AXA6 <Abdullah97sd@gmail.com>
2026-02-28 03:57:21 +0000

    SQL Introduction: Add all task scripts

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates the table first_table in the current database
+-- If the table already exists, the script will not fail
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
